05d6f952f33ac4b1e45cf2a95ee05337bb9b0d18
d0riven <wega315@gmail.com>
2020-07-13 21:33:59 +0900

    test: Add arg ApplicationSpec spec

--- spec/ApplicatonSpec.spec.php
+++ spec/ApplicatonSpec.spec.php
@@ -167,7 +167,136 @@ describe('ApplicationSpec', function () {
                 });
             });
         });
+    });
 
+    describe('Arg', function () {
+        describe('int', function () {
+            beforeEach(function () {
+                $spec = $this->spec; /** @var ApplicationSpec $spec */
+                $this->val = $spec->arg()->default(3)->int('v');
+            });
+
+            context('when exists arg with value 1', function () {
+                $argv = explode(' ', 'test.php 1');
+                it('return int 1', function () use ($argv) {
+                    PhpFlags\Parser::create($this->spec)->parse($argv);
+                    expect($this->val->get())->toBeA('integer');
+                    expect($this->val->get())->toBe(1);
+                });
+            });
+            context('when no arg', function () {
+                $argv = explode(' ', 'test.php');
+                it('return default int 3', function () use ($argv) {
+                    PhpFlags\Parser::create($this->spec)->parse($argv);
+                    expect($this->val->get())->toBeA('integer');
+                    expect($this->val->get())->toBe(3);
+                });
+            });
+        });
+
+        describe('int', function () {
+            beforeEach(function () {
+                $spec = $this->spec; /** @var ApplicationSpec $spec */
+                $this->val = $spec->arg()->default(3.3)->float('v');
+            });
+
+            context('when exists arg with value 1.1', function () {
+                $argv = explode(' ', 'test.php 1.1');
+                it('return float 1.1', function () use ($argv) {
+                    PhpFlags\Parser::create($this->spec)->parse($argv);
+                    expect($this->val->get())->toBeA('float');
+                    expect($this->val->get())->toBe(1.1);
+                });
+            });
+            context('when no arg', function () {
+                $argv = explode(' ', 'test.php');
+                it('return default float 3.3', function () use ($argv) {
+                    PhpFlags\Parser::create($this->spec)->parse($argv);
+                    expect($this->val->get())->toBeA('float');
+                    expect($this->val->get())->toBe(3.3);
+                });
+            });
+        });
+
+        describe('string', function () {
+            beforeEach(function () {
+                $spec = $this->spec; /** @var ApplicationSpec $spec */
+                $this->val = $spec->arg()->default('def')->string('v');
+            });
+
+            context('when exists arg with value str', function () {
+                $argv = explode(' ', 'test.php str');
+                it('return string "str"', function () use ($argv) {
+                    PhpFlags\Parser::create($this->spec)->parse($argv);
+                    expect($this->val->get())->toBeA('string');
+                    expect($this->val->get())->toBe('str');
+                });
+            });
+            context('when no arg', function () {
+                $argv = explode(' ', 'test.php');
+                it('return default string "def"', function () use ($argv) {
+                    PhpFlags\Parser::create($this->spec)->parse($argv);
+                    expect($this->val->get())->toBeA('string');
+                    expect($this->val->get())->toBe('def');
+                });
+            });
+        });
+
+        describe('date', function () {
+            beforeEach(function () {
+                $spec = $this->spec; /** @var ApplicationSpec $spec */
+                $this->val = $spec->arg()->default(new DateTimeImmutable('2020-02-01'))->date('v');
+            });
+
+            context('when exists arg with value 2000-01-01', function () {
+                $argv = explode(' ', 'test.php 2020-01-01');
+                it('return string "str"', function () use ($argv) {
+                    PhpFlags\Parser::create($this->spec)->parse($argv);
+                    expect($this->val->get())->toBeAnInstanceOf('DateTimeImmutable');
+                    expect($this->val->get()->format(DATE_ATOM))->toBe('2020-01-01T00:00:00+00:00');
+                });
+            });
+            context('when no arg', function () {
+                $argv = explode(' ', 'test.php');
+                it('return default string "def"', function () use ($argv) {
+                    PhpFlags\Parser::create($this->spec)->parse($argv);
+                    expect($this->val->get())->toBeAnInstanceOf('DateTimeImmutable');
+                    expect($this->val->get()->format(DATE_ATOM))->toBe('2020-02-01T00:00:00+00:00');
+                });
+            });
+        });
+
+        describe('bool', function () {
+            beforeEach(function () {
+                $spec = $this->spec; /** @var ApplicationSpec $spec */
+                $this->val = $spec->arg()->default(true)->bool();
+            });
+
+            context('when exists arg with value true', function () {
+                $argv = explode(' ', 'test.php true');
+                it('return bool true', function () use ($argv) {
+                    PhpFlags\Parser::create($this->spec)->parse($argv);
+                    expect($this->val->get())->toBeA('boolean');
+                    expect($this->val->get())->toBe(true);
+                });
+            });
+            context('when exists arg with value false', function () {
+                $argv = explode(' ', 'test.php false');
+                it('return bool false', function () use ($argv) {
+                    PhpFlags\Parser::create($this->spec)->parse($argv);
+                    expect($this->val->get())->toBeA('boolean');
+                    expect($this->val->get())->toBe(false);
+                });
+            });
+            context('when no arg', function () {
+                $argv = explode(' ', 'test.php');
+                it('return default bool true', function () use ($argv) {
+                    PhpFlags\Parser::create($this->spec)->parse($argv);
+                    expect($this->val->get())->toBeA('boolean');
+                    expect($this->val->get())->toBe(true);
+                });
+            });
+        });
     });
 });
 
