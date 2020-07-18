43319b29fa514fc2d18c6103c930f30d9513ab02
d0riven <wega315@gmail.com>
2020-07-18 23:39:08 +0900

    test: add ping scenario spec

--- spec/ApplicatonSpec.spec.php
+++ spec/ApplicatonSpec.spec.php
@@ -7,7 +7,70 @@ describe('feature parse based on the ApplicationSpec', function () {
         $this->spec = new ApplicationSpec();
     });
 
-    describe('scenario Flag', function () {
+    describe('scenario ping', function () {
+        beforeEach(function () {
+            $spec = $this->spec;
+            /** @var ApplicationSpec $spec */
+            $this->count = $spec->flag('count')->short('c')->default(-1)
+                ->desc('Number of times to send an ICMP request. The default of -1 sends an unlimited number of requests.')
+                ->int('request count');
+            $this->timeout = $spec->flag('timeout')->short('t')->default(5)
+                ->desc('Timeout seconds for ICMP requests.')
+                ->int('request count');
+            $this->verbose = $spec->flag('verbose')->short('V')
+                ->desc('verbose output.')
+                ->bool();
+            $this->host = $spec->arg()
+                ->desc('IP of the host for the ICMP request.')
+                ->string('host');
+        });
+
+        context('when only arg host ip', function() {
+            $argv = explode(' ', 'ping 127.0.0.1');
+            it('count is default int -1, timeout is default 5, verbose is false, and host is 127.0.0.1', function () use ($argv) {
+                PhpFlags\Parser::create($this->spec)->parse($argv);
+                expect($this->count->get())->toBe(-1);
+                expect($this->timeout->get())->toBe(5);
+                expect($this->verbose->get())->toBe(false);
+                expect($this->host->get())->toBe('127.0.0.1');
+            });
+        });
+
+        context('when exists count short -c flag with value of 10', function() {
+            $argv = explode(' ', 'ping -c 10 127.0.0.1');
+            it('count is int 10, timeout is default 5, verbose is false, and host is 127.0.0.1', function () use ($argv) {
+                PhpFlags\Parser::create($this->spec)->parse($argv);
+                expect($this->count->get())->toBe(10);
+                expect($this->timeout->get())->toBe(5);
+                expect($this->verbose->get())->toBe(false);
+                expect($this->host->get())->toBe('127.0.0.1');
+            });
+        });
+
+        context('when exists count short -c flag with value of 10, and timeout long short -t flag with value 1', function() {
+            $argv = explode(' ', 'ping -c 10 -t=1 127.0.0.1');
+            it('return count int 10, timeout is 1, verbose is false, and host 127.0.0.1', function () use ($argv) {
+                PhpFlags\Parser::create($this->spec)->parse($argv);
+                expect($this->count->get())->toBe(10);
+                expect($this->timeout->get())->toBe(1);
+                expect($this->verbose->get())->toBe(false);
+                expect($this->host->get())->toBe('127.0.0.1');
+            });
+        });
+
+        context('when exists count short -c flag with value of 10, timeout long short -t flag with value 1, and -V flag', function() {
+            $argv = explode(' ', 'ping -c 10 -t=1 -V 127.0.0.1');
+            it('return count int 10, timeout is 1, verbose is true, and host 127.0.0.1', function () use ($argv) {
+                PhpFlags\Parser::create($this->spec)->parse($argv);
+                expect($this->count->get())->toBe(10);
+                expect($this->timeout->get())->toBe(1);
+                expect($this->verbose->get())->toBe(true);
+                expect($this->host->get())->toBe('127.0.0.1');
+            });
+        });
+    });
+
+    describe('Flag', function () {
         context('int', function () {
             beforeEach(function () {
                 $spec = $this->spec;
@@ -19,7 +82,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php --long 1');
                 it('return int value 1', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('integer');
                     expect($this->val->get())->toBe(1);
                 });
             });
@@ -27,7 +89,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php -s 2');
                 it('return int value 1', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('integer');
                     expect($this->val->get())->toBe(2);
                 });
             });
@@ -35,7 +96,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php');
                 it('return default int value 3', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('integer');
                     expect($this->val->get())->toBe(3);
                 });
             });
@@ -52,7 +112,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php --long 1 -s 2');
                 it('return int values [1, 2]', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('array');
                     expect($this->val->get())->toBe([1, 2]);
                 });
             });
@@ -60,7 +119,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php');
                 it('return default int values [1, 2, 3]', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('array');
                     expect($this->val->get())->toBe([1, 2, 3]);
                 });
             });
@@ -77,7 +135,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php --long 1.1');
                 it('return float value 1.1', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('float');
                     expect($this->val->get())->toBe(1.1);
                 });
             });
@@ -85,7 +142,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php -s 2.2');
                 it('return float value 2.2', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('float');
                     expect($this->val->get())->toBe(2.2);
                 });
             });
@@ -93,7 +149,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php');
                 it('return default float value 3.3', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('float');
                     expect($this->val->get())->toBe(3.3);
                 });
             });
@@ -110,7 +165,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php --long long');
                 it('return string long', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('string');
                     expect($this->val->get())->toBe('long');
                 });
             });
@@ -118,7 +172,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php -s short');
                 it('return string short', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('string');
                     expect($this->val->get())->toBe('short');
                 });
             });
@@ -126,7 +179,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php');
                 it('return default string def', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('string');
                     expect($this->val->get())->toBe('def');
                 });
             });
@@ -176,7 +228,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php --long');
                 it('return true', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('boolean');
                     expect($this->val->get())->toBe(true);
                 });
             });
@@ -184,7 +235,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php -s');
                 it('return true', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('boolean');
                     expect($this->val->get())->toBe(true);
                 });
             });
@@ -192,7 +242,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php');
                 it('return false', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('boolean');
                     expect($this->val->get())->toBe(false);
                 });
             });
@@ -212,7 +261,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php 1');
                 it('return int 1', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('integer');
                     expect($this->val->get())->toBe(1);
                 });
             });
@@ -220,7 +268,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php');
                 it('return default int 3', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('integer');
                     expect($this->val->get())->toBe(3);
                 });
             });
@@ -237,7 +284,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php 1');
                 it('return int values [1]', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('array');
                     expect($this->val->get())->toBe([1]);
                 });
             });
@@ -245,7 +291,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php 1 2');
                 it('return int values [1, 2]', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('array');
                     expect($this->val->get())->toBe([1, 2]);
                 });
             });
@@ -253,7 +298,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php');
                 it('return default int values [1, 2, 3]', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('array');
                     expect($this->val->get())->toBe([1, 2, 3]);
                 });
             });
@@ -270,7 +314,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php 1.1');
                 it('return float 1.1', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('float');
                     expect($this->val->get())->toBe(1.1);
                 });
             });
@@ -278,7 +321,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php');
                 it('return default float 3.3', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('float');
                     expect($this->val->get())->toBe(3.3);
                 });
             });
@@ -295,7 +337,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php str');
                 it('return string "str"', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('string');
                     expect($this->val->get())->toBe('str');
                 });
             });
@@ -303,7 +344,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php');
                 it('return default string "def"', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('string');
                     expect($this->val->get())->toBe('def');
                 });
             });
@@ -345,7 +385,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php true');
                 it('return bool true', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('boolean');
                     expect($this->val->get())->toBe(true);
                 });
             });
@@ -353,7 +392,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php false');
                 it('return bool false', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('boolean');
                     expect($this->val->get())->toBe(false);
                 });
             });
@@ -361,7 +399,6 @@ describe('feature parse based on the ApplicationSpec', function () {
                 $argv = explode(' ', 'test.php');
                 it('return default bool true', function () use ($argv) {
                     PhpFlags\Parser::create($this->spec)->parse($argv);
-                    expect($this->val->get())->toBeA('boolean');
                     expect($this->val->get())->toBe(true);
                 });
             });
